747c9e57f3ab898b55530ea456def7402128c82d
and-re-s <andre.syromyatnikov@gmail.com>
2022-09-10 17:31:47 +0600

    Refactored user reducer to TypeScript

--- src/store/user/user.reducer.ts
+++ src/store/user/user.reducer.ts
@@ -1,9 +1,17 @@
-import { USER_ACTION_TYPES } from "./user.types";
+import { AnyAction } from "redux";
+import {
+  signInSuccess,
+  signOutSuccess,
+  signOutFailed,
+  signInFailed,
+  signUpFailed,
+} from "./user.action";
+import { UserData } from "../../utils/firebase/firebase.utils";
 
 export type UserState = {
-  currentUser: any;
-  isLoading: boolean;
-  error: any;
+  readonly currentUser: UserData | null;
+  readonly isLoading: boolean;
+  readonly error: Error | null;
 };
 
 const INITIAL_STATE: UserState = {
@@ -12,19 +20,22 @@ const INITIAL_STATE: UserState = {
   error: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-      return { ...state, currentUser: payload };
-    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
-      return { ...state, currentUser: null };
-    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
-    case USER_ACTION_TYPES.SIGN_IN_FAILED:
-    case USER_ACTION_TYPES.SIGN_UP_FAILED:
-      return { ...state, error: payload };
-    default:
-      return state;
+export const userReducer = (state = INITIAL_STATE, action: AnyAction) => {
+  if (signInSuccess.match(action)) {
+    return { ...state, currentUser: action.payload };
   }
+
+  if (signOutSuccess.match(action)) {
+    return { ...state, currentUser: null };
+  }
+
+  if (
+    signOutFailed.match(action) ||
+    signInFailed.match(action) ||
+    signUpFailed.match(action)
+  ) {
+    return { ...state, error: action.payload };
+  }
+
+  return state;
 };
--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,3 +1,10 @@
 import { UserState } from "./user.reducer";
 
-export const selectCurrentUser = (state): UserState => state.user.currentUser;
+import { createSelector } from "reselect";
+
+export const selectUserReducer = (state): UserState => state.user;
+
+export const selectCurrentUser = createSelector(
+  selectUserReducer,
+  (user) => user.currentUser
+);
